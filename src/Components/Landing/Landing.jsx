7ba3f6395e1d18caccca6f408b6c1f3390f24fdd
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-16 20:45:28 +1300

    connect firestore to app

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Landing.css'
 import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
 import { auth } from '../../firebase-config'
 import { Link } from 'react-router-dom'
+import { db } from '../../firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
+
 
 
 const Landing = () => {
 
    const [user, setUser] = useState({});
+   const [projectData, setProjectData] = useState([])
    const userAuth = getAuth();
    const activeUser = userAuth.currentUser;
 
    const logout = async () => {
       await signOut(auth);
    };
+
+   const projectCollectionRef = collection(db, 'projects')
+
+   useEffect(() => { 
+      const getProjects = async () => {
+         const data = await getDocs(projectCollectionRef);
+         setProjectData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+      }
+      getProjects();
+   }, []);
 
    return (
       <div className="landing-container">
